6328e4b1f41829bd910b68fab5971b1ce8636609
Khadija Dieng <khadijadieng1996@gmail.com>
2026-07-27 15:47:39 +0000

    chore(campushub): initialiser le projet Spring Boot

---
+++ src/test/java/com/ept/campushub/CampushubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ept.campushub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampushubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
