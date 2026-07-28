6c3a96db0529d3343f586c1891f0b6be6e1d6ab1
User <saissatou@ept.sn>
2026-07-28 15:08:37 +0000

    feat(routing): configuration des routes dans app.route.ts

---
+++ campushub-front/src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { EtudiantListComponent } from './components/etudiant-list-component/etudiant-list-component';
+import { EtudiantFormComponent } from './components/etudiant-form-component/etudiant-form-component';
+import { EtudiantDetailComponent } from './components/etudiant-detail-component/etudiant-detail-component';
+import { NotFoundComponent } from './components/not-found-component/not-found-component';
+
+export const routes: Routes = [
+  // Redirection par défaut vers /etudiants
+  { path: '', redirectTo: 'etudiants', pathMatch: 'full' },
+
+  // Route 1 : Liste
+  { path: 'etudiants', component: EtudiantListComponent },
+
+  // Route 2 : Formulaire de création (OBLIGATOIRE avant :id)
+  { path: 'etudiants/nouveau', component: EtudiantFormComponent },
+
+  // Route 3 : Fiche détaillée d'un étudiant
+  { path: 'etudiants/:id', component: EtudiantDetailComponent },
+
+  // Route 4 : Page 404 (toujours à la fin)
+  { path: '**', component: NotFoundComponent },
+];
--- campushub-front/src/app/app.ts
+++ campushub-front/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { EtudiantListComponent } from "./components/etudiant-list-component/etudiant-list-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EtudiantListComponent,],
+  imports: [RouterOutlet, EtudiantListComponent, RouterLinkWithHref],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
