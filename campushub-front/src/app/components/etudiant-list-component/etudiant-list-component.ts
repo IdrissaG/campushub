d816e917a05818fc23e0b9dcc3179968386f5794
fatoumory <fcndong@ept.sn>
2026-07-28 13:10:12 +0000

    feat(etudiant-list): Crèation du composant etudiant-list

---
+++ campushub-front/src/app/components/etudiant-list-component/etudiant-list-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-etudiant-list-component',
+  imports: [],
+  templateUrl: './etudiant-list-component.html',
+  styleUrl: './etudiant-list-component.scss',
+})
+export class EtudiantListComponent {}
