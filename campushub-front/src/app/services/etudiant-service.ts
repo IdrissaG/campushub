280d83ea2dbb9f37a1903f5779eefdf9e5a627fe
fatoumory <fcndong@ept.sn>
2026-07-29 10:45:11 +0000

    feat(etudiant-service: Ajout du contenu de EtudiantService

--- campushub-front/src/app/services/etudiant-service.ts
+++ campushub-front/src/app/services/etudiant-service.ts
@@ -1,4 +1,15 @@
-import { Service } from '@angular/core';
+import { inject, Service } from '@angular/core';
+import { Etudiant } from '../model/etudiant.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Service()
-export class EtudiantService {}
+export class EtudiantService {
+    private apiUrl = 'http://localhost:8080/api/etudiants';
+
+    private http = inject(HttpClient);
+
+    getAll(): Observable<Etudiant[]> {
+       return this.http.get<Etudiant[]>(this.apiUrl);
+    }
+}
