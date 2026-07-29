852f538e104c503cde00a955ce4e43929b2bf08b
mohamedodiallo <modiallo@ept.sn>
2026-07-29 15:56:12 +0000

    feat(form): modifications supplementaires

--- campushub-front/src/app/components/etudiant-list-component/etudiant-list-component.ts
+++ campushub-front/src/app/components/etudiant-list-component/etudiant-list-component.ts
@@ -9,10 +9,10 @@ import { EtudiantCardComponent } from '../etudiant-card-component/etudiant-card-
   styleUrl: './etudiant-list-component.scss',
 })
 export class EtudiantListComponent {
-  etudiants : Etudiant[] = [
-  { id: 1, nom: 'Diop', prenom: 'Awa', filiere: 'Info' },
-  { id: 2, nom: 'Fall', prenom: 'Moussa', filiere: 'Gestion' },
-  {id: 3, nom:'Ndiaye', prenom:'Astou', filiere:'Chimie'},
-  {id: 4, nom:'Faye', prenom:'Aly', filiere:'Maths'}
-  ]
+  etudiants: Etudiant[] = [
+  { id: 1, nom: 'Diop',   prenom: 'Awa',    filiere: 'Info',    email: 'awa.diop@gmail.com' },
+  { id: 2, nom: 'Fall',   prenom: 'Moussa', filiere: 'Gestion', email: 'moussa.fall@gmail.com' },
+  { id: 3, nom: 'Ndiaye', prenom: 'Astou',  filiere: 'Chimie',  email: 'astou.ndiaye@gmail.com' },
+  { id: 4, nom: 'Faye',   prenom: 'Aly',    filiere: 'Maths',   email: 'aly.faye@gmail.com' },
+];
 }
