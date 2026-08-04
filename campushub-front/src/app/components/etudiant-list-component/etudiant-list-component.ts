38b00df6fceb2c71ffd149276a14c0eaaf083f64
sowibrr <ibous2005@gmail.com>
2026-08-04 09:28:20 +0000

    refactor(etudiant): Ajout d'un état de chargement dans le composant etudiant list

--- campushub-front/src/app/components/etudiant-list-component/etudiant-list-component.ts
+++ campushub-front/src/app/components/etudiant-list-component/etudiant-list-component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Etudiant } from '../../model/etudiant.interface';
 import { EtudiantCardComponent } from '../etudiant-card-component/etudiant-card-component';
 import { EtudiantService } from '../../services/etudiant-service';
+import { PageResponse } from '../../model/page-response.model';
 
 @Component({
   selector: 'app-etudiant-list-component',
@@ -14,10 +15,19 @@ export class EtudiantListComponent implements OnInit{
   etudiants: Etudiant[] = [];
   private etudiantService = inject(EtudiantService);
 
+  loading = signal(true);
+  erreur = signal<string | null>(null);
+
   ngOnInit() {
     this.etudiantService.getAll().subscribe({
-      next: (data) => this.etudiants = data,
-      error: (err) => console.error('Erreur API', err)
+      next: (data) => {
+        this.etudiants = data.content;
+        this.loading.set(false);
+      },
+      error: (err) => {
+        this.erreur.set('Impossible de charger les étudiants');
+        this.loading.set(false);
+      }
     });
 }
 }
