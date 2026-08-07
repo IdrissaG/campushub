abd818cfb1f5a9b4794b066fbb0c12a2a7e03d36
User <saissatou@ept.sn>
2026-08-07 10:02:56 +0000

    fix(routing): rectification

--- campushub-front/src/app/app.routes.ts
+++ campushub-front/src/app/app.routes.ts
@@ -5,19 +5,10 @@ import { EtudiantDetailComponent } from './components/etudiant-detail-component/
 import { NotFoundComponent } from './components/not-found-component/not-found-component';
 
 export const routes: Routes = [
-  // Redirection par défaut vers /etudiants
   { path: '', redirectTo: 'etudiants', pathMatch: 'full' },
-  
-
-  // Route 1 : Liste
   { path: 'etudiants', component: EtudiantListComponent },
-
-  // Route 2 : Formulaire de création 
-  { path: 'etudiants/nouveau', component: EtudiantFormComponent },
-
-  // Route 3 : Fiche détaillée d'un étudiant
   { path: 'etudiants/:id', component: EtudiantDetailComponent },
-
-  // Route 4 : Page 404 
+  { path: 'etudiants/:id/modifier', component: EtudiantFormComponent },
+  { path: 'etudiants/nouveau', component: EtudiantFormComponent },
   { path: '**', component: NotFoundComponent },
 ];
