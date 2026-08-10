b256ff3c80af85eb2ec4dc6d7500e94436723782
mohamedodiallo <modiallo@ept.sn>
2026-08-10 08:55:36 +0000

    feat(auth): ajout interfaces LoginRequest et AuthResponse dans autht.interface.ts

---
+++ campushub-front/src/app/model/auth.interface.ts
@@ -0,0 +1,11 @@
+// Ce qu'on ENVOIE à G5 pour se connecter
+export interface LoginRequest {
+  email: string;
+  motDePasse: string;
+}
+
+// Ce que G5 RENVOIE après connexion réussie
+export interface AuthResponse {
+  token: string; // JWT à inclure dans chaque requête protégée
+  role: string;  // 'ADMIN' ou 'ETUDIANT'
+}
